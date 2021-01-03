333f487221a90e167dc011afc795e5094f834bbb
D-Gould <danagould32@gmail.com>
2021-01-02 18:04:43 -0800

    Added jquery and shifted game play to the server side

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 var server = http.Server(app)
 var io = socketIO(server)
 
-app.set('port', 5000);
+app.set('port', 3000);
 app.use('/static', express.static(__dirname + '/static'));
 
 // Routing
@@ -17,14 +17,24 @@ app.get('/', function(request, response) {
 });
 
 // Starts the server.
-server.listen(5000, function() {
-    console.log('Starting server on port 5000');
+server.listen(3000, function() {
+    console.log('Starting server on port 3000');
 });
+
+function checkForWinner() {
+    players.forEach( function(player) {
+        if (player.score >= 100) {
+            return player
+        }
+    })
+    console.log("check for winner works")
+    return false
+}
 
 var players = []
 io.on('connection', function(socket) {
-    console.log("connected!")
     const colors = ['red', 'green', 'blue', 'purple']
+    const turnIndex = 0
     socket.on('new player', function(name) {
         players.push({
             id: socket.id,
@@ -32,12 +42,39 @@ io.on('connection', function(socket) {
             name: name,
             score: 0
         })
-        console.log("new player!", players)
         io.sockets.emit('updatePlayers', players);
     });
-    // socket.on('players', function() {
-    //     io.sockets.emit('updatePlayers', players)
-    // })
-
+    socket.on('start game', function() {
+        io.sockets.emit('no new players', players)
+        // loop through players, give each a turn
+        // loop until player chooses to "Stay" || rolls a pig out
+        console.log("Game Starting Now!")
+        let turnIndex = 0;
+        while (!checkForWinner()) {
+            console.log("First turn")
+            let turnTotal = 0
+            socket.emit('update turn', players[turnIndex])
+            
+            // display buttons: "Throw the pigs!" "Stay"
+            // if player clicks "Stay":
+                // socket.emit('updateScore') - player.score += turn total
+                // update score board
+                // next turn
+            // if player clicks "Throw the pigs":
+                // throw the pigs
+                // if pig out:  next turn
+                // if Oinker: 
+                    // socket.emit('updateScore') - player.score += turn total
+                    // update score board
+                    // next turn
+                // else: display the throw, update Turn total
+            break
+    }
+    });
+    // socket.on('next turn', function() {
+    //    turnIndex === 0 ? 0 : turnIndex++ % players.size
+    //    console.log("turnIndex", turnIndex)
+    //    io.sockets.emit('update turn', players[turnIndex])
+    // });
    
 })
--- static/game.js
+++ static/game.js
@@ -1,40 +1,24 @@
 var socket = io();
 
 let gamePlayers = []
-
+$(document).ready(function() {
+    $('#startButton').click(function(event) {
+        socket.emit('start game')
+    })
+})  
 function updateScoreTable(players) {
-    console.log("building score table")
-    let scoreTable = document.getElementById('scoreTable') 
-    scoreTable.innerHTML = ""
+    let scoreTable = $("#scoreTable")
+    $("#scoreTable tr").remove()
     players.forEach(player =>  {
-        let row = scoreTable.insertRow(-1)
-        let name = row.insertCell(0)
-        let score = row.insertCell(1)
-        name.innerHTML = player.name
-        score.innerHTML = player.score
+        scoreTable.append(`<tr><td>${player.name}</td><td>${player.score}</td></tr>`)
     });
 }
-function playGame() {
-
-    // loop through players, give each a turn
-    // loop until player chooses to "Stay" || rolls a pig out
-    // while (!turnOver) {
-        // display "It's Player's turn"
-        // create & display Turn total
-        // display buttons: "Throw the pigs!" "Stay"
-        // if player clicks "Stay":
-            // socket.emit('updateScore') - player.score += turn total
-            // update score board
-            // next turn
-        // if player clicks "Throw the pigs":
-            // throw the pigs
-            // if pig out:  next turn
-            // if Oinker: 
-                // socket.emit('updateScore') - player.score += turn total
-                // update score board
-                // next turn
-            // else: display the throw, update Turn total
+function updateTurn(player) {
+    $('#turn').show()
+    $("#turnName").html(`It is ${player.name}'s turn.`)
+    $("#turnTotal").html('Turn Total: 0')
 }
+
 // Allow players to join the game
 let nameButton = document.getElementById('nameButton') 
 nameButton.addEventListener('click', function(event) {
@@ -43,23 +27,24 @@ nameButton.addEventListener('click', function(event) {
 });
 
 // Hide the ability to add new players // Start the game
-let startButton = document.getElementById('startButton') 
-startButton.addEventListener('click', function(event) {
-    document.getElementById('addPlayers').style.visibility = 'hidden'
-    document.getElementById('startGame').style.visibility = 'hidden'
-    playGame()
-});
+$(document).ready(function() {
+    $('#startButton').click(function(event) {
+        socket.emit('start game')
+    })
+})  
 
-
-// Socket responses
-// socket.on('updateScore', function() {
-//     updateScoreTable(gamePlayers)
-// })
-
-socket.on('updatePlayers', function(players) {
-    console.log("players = ", players)
-    gamePlayers = players
-    updateScoreTable(gamePlayers)
-    console.log("gamePlayers = ", gamePlayers)
+socket.on('no new players', function() {
+    $(document).ready(function() {
+        $("#gamePrep").hide()
+        $('#startGame').hide()
+    })
 })
 
+socket.on('updatePlayers', function(players) {
+    gamePlayers = players
+    updateScoreTable(gamePlayers)
+});
+
+socket.on('update turn', function(player) {
+    updateTurn(player)
+});
