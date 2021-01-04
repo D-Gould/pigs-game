e02cff9091095ca26a28cf7ec9c10a1243822530
D-Gould <danagould32@gmail.com>
2021-01-03 20:15:12 -0800

    finished up the game

--- server.js
+++ server.js
@@ -20,21 +20,85 @@ app.get('/', function(request, response) {
 server.listen(3000, function() {
     console.log('Starting server on port 3000');
 });
+let players = []
+let turnIndex = 0
+let turnTotal = 0
 
-function checkForWinner() {
+function isThereAWinner() {
+    let winner;
     players.forEach( function(player) {
         if (player.score >= 100) {
-            return player
+            winner = player
         }
     })
-    console.log("check for winner works")
-    return false
+    return winner
+}
+const oinker = () => {
+    // approx 1 in 30 rolls should be an oinker
+    return Math.floor((Math.random() * 30)) === 1
+}
+const score = ([pig1, pig2]) => {
+    if (pig1.name === pig2.name && pig1.name.match('Side')) {
+        return 1
+    } else if (pig1.name === pig2.name) {
+        return (pig1.points + pig2.points)*2
+    } else { 
+        return pig1.points + pig2.points
+    }
+}
+const randomRoll = () => {
+    const num=Math.random();
+    if(num < 0.35) return 5;  // Side probability 0.35 
+    else if(num < 0.65) return 4; // Dot Side probability 0.30
+    else if(num < 0.87) return 0; // Razorback probability 0.22
+    else if(num < 0.96) return 1; // Trotter probability 0.09
+    else if(num < 0.99) return 2; // Snouter probability 0.03
+    else return 3;  // Leaning Jowler probability 0.01
+}
+const roll = () => {
+    if (oinker()) {
+        return {type: "oinker", pig1: {name: "Oinker!"}, pig2: {name: "Oinker!"}, score: 0}
+    }
+
+    // TODO: Add images into each pig
+    let pigOptions = [{name: "Razorback", points: 5}, 
+                    {name: "Trotter", points: 5}, 
+                    {name: "Snouter", points: 10}, 
+                    {name: "Leaning Jowler", points: 15}, 
+                    {name: "Dot Side", points: 0}, 
+                    {name: "Side", points: 0}]
+    
+    // TODO: figure out how to add probability into the random rolls
+    const pigs = [pigOptions[randomRoll()], pigOptions[Math.floor(randomRoll())]]
+    
+    if (pigs.find(pig => pig.name === "Dot Side") && pigs.find(pig => pig.name === "Side")) {
+        return {type: "pig out", pig1: pigs[0], pig2: pigs[1], score: 0}
+    }
+    const rollScore = score(pigs)
+    return {type: "roll", pig1: pigs[0], pig2: pigs[1], score: rollScore}
 }
 
-var players = []
+function nextTurn() {
+    // Add last player's turntotal to their score
+    players[turnIndex].score += turnTotal
+    io.sockets.emit("update score table", players)
+    
+    // Check if anyone has won
+    const winner = isThereAWinner()    
+    if (winner) {
+        io.sockets.emit("winner", winner)
+    }
+
+    // Set up the next player
+    turnTotal = 0
+    turnIndex += 1
+    turnIndex = turnIndex % players.length
+    // update the board
+    io.sockets.emit("update turn", players[turnIndex])
+}
+
 io.on('connection', function(socket) {
     const colors = ['red', 'green', 'blue', 'purple']
-    const turnIndex = 0
     socket.on('new player', function(name) {
         players.push({
             id: socket.id,
@@ -42,39 +106,50 @@ io.on('connection', function(socket) {
             name: name,
             score: 0
         })
-        io.sockets.emit('updatePlayers', players);
+        io.sockets.emit('update score table', players);
+        if (players.length >=2) {
+            io.sockets.emit('show start button');
+
+        }
     });
     socket.on('start game', function() {
-        io.sockets.emit('no new players', players)
-        // loop through players, give each a turn
-        // loop until player chooses to "Stay" || rolls a pig out
-        console.log("Game Starting Now!")
-        let turnIndex = 0;
-        while (!checkForWinner()) {
-            console.log("First turn")
-            let turnTotal = 0
-            socket.emit('update turn', players[turnIndex])
-            
-            // display buttons: "Throw the pigs!" "Stay"
-            // if player clicks "Stay":
-                // socket.emit('updateScore') - player.score += turn total
-                // update score board
-                // next turn
-            // if player clicks "Throw the pigs":
-                // throw the pigs
-                // if pig out:  next turn
-                // if Oinker: 
-                    // socket.emit('updateScore') - player.score += turn total
-                    // update score board
-                    // next turn
-                // else: display the throw, update Turn total
-            break
-    }
+        io.sockets.emit('turn off new players', players)
+        io.sockets.emit("update turn", players[turnIndex])
     });
-    // socket.on('next turn', function() {
-    //    turnIndex === 0 ? 0 : turnIndex++ % players.size
-    //    console.log("turnIndex", turnIndex)
-    //    io.sockets.emit('update turn', players[turnIndex])
-    // });
+    socket.on('roll the pigs', function() {
+        let thisRoll = roll();
+        let player = players[turnIndex]
+        switch (thisRoll.type) {
+            case "pig out":
+                turnTotal = 0
+                io.sockets.emit("update turnTotal", turnTotal)
+                io.sockets.emit("update roll", thisRoll)
+                io.sockets.emit("pig out", player)
+                break;
+            case "oinker": 
+                player.score = 0
+                io.sockets.emit("update score table", players)
+                turnTotal = 0
+                io.sockets.emit("update turnTotal", turnTotal)
+                io.sockets.emit("update roll", thisRoll)
+                io.sockets.emit("oinker", player)
+                break;
+            case "roll": 
+                io.sockets.emit("update roll", thisRoll)
+                turnTotal += thisRoll.score
+                io.sockets.emit("update turn total", turnTotal)
+                break;
+        }
+    })
+    socket.on('pass to next player', function() {
+        nextTurn()
+    })
+    socket.on('reset game', function() {
+        players = [];
+        turnTotal = 0
+        turnIndex = 0
+        io.sockets.emit("update score table", players)
+        io.sockets.emit("reset")
+    })
    
 })
--- static/game.js
+++ static/game.js
@@ -1,50 +1,136 @@
 var socket = io();
 
-let gamePlayers = []
-$(document).ready(function() {
-    $('#startButton').click(function(event) {
-        socket.emit('start game')
-    })
-})  
-function updateScoreTable(players) {
-    let scoreTable = $("#scoreTable")
-    $("#scoreTable tr").remove()
-    players.forEach(player =>  {
-        scoreTable.append(`<tr><td>${player.name}</td><td>${player.score}</td></tr>`)
-    });
-}
-function updateTurn(player) {
-    $('#turn').show()
-    $("#turnName").html(`It is ${player.name}'s turn.`)
-    $("#turnTotal").html('Turn Total: 0')
-}
-
-// Allow players to join the game
-let nameButton = document.getElementById('nameButton') 
-nameButton.addEventListener('click', function(event) {
-    let name = document.getElementById("name").value
-    socket.emit('new player', name);
-});
-
-// Hide the ability to add new players // Start the game
+// Start the game when the start button is clicked
 $(document).ready(function() {
     $('#startButton').click(function(event) {
         socket.emit('start game')
     })
 })  
 
-socket.on('no new players', function() {
+// When "join" button is clicked, player joins the game
+$(document).ready(function() {
+    $('#nameButton').click(function(event) {
+        let name = $("#name").val()
+        socket.emit('new player', name);
+    })
+})
+
+// Start the game
+$(document).ready(function() {
+    $('#startButton').click(function(event) {
+        socket.emit('start game')
+    })
+})  
+
+// Pass to next player
+$(document).ready(function() {
+    $('#stayButton').click(function(event) {
+        socket.emit('pass to next player')
+    })
+})  
+
+// Roll the Pigs
+$(document).ready(function() {
+    $('#rollButton').click(function(event) {
+        socket.emit('roll the pigs')
+    })
+})  
+// Show the start button
+socket.on('show start button', function() {
+    $(document).ready(function() {
+        $("#startButton").show()
+        $('#titleMessage').html("When all the players have joined, click 'Start the Game'")
+    })
+})
+// Remove elements that allow new players to be added
+socket.on('turn off new players', function() {
     $(document).ready(function() {
         $("#gamePrep").hide()
         $('#startGame').hide()
     })
 })
 
-socket.on('updatePlayers', function(players) {
-    gamePlayers = players
-    updateScoreTable(gamePlayers)
+function updateScoreTable(players) {
+    let scoreTable = $("#scoreTable")
+    $("#scoreTable tr").remove()
+    scoreTable.append(`<tr><th>Name</th><th>Points</th></tr>`)
+    players.forEach(player =>  {
+        scoreTable.append(`<tr><td>${player.name}</td><td>${player.score}</td></tr>`)
+    });
+}
+// Update score display
+socket.on('update score table', function(players) {
+    updateScoreTable(players)
 });
 
+function updateTurn(player) {
+    $('.message').hide()
+    $('#turn').show()
+    $('#rollButton').show()
+    $('#roll').hide()
+    $('#stayButton').hide()
+    $("#turnName").html(`It is ${player.name}'s turn.  `)
+    $("#turnTotal").html('Turn Total: 0')
+}
+// Update turn display
 socket.on('update turn', function(player) {
     updateTurn(player)
 });
+
+function updateRoll(roll) {
+    $("#pig1Name").html(`${roll.pig1.name}`)
+    $("#pig2Name").html(`${roll.pig2.name}`)
+    $("#rollTotal").html(`This roll is worth ${roll.score} points`)
+    $('#roll').show()
+}
+// Update roll display
+socket.on('update roll', function(roll) {
+    updateRoll(roll)
+    $('#stayButton').show()
+
+});
+// Update turn total
+socket.on('update turn total', function(turnTotal) {
+    $("#turnTotal").html(`Turn Total: ${turnTotal}`)
+});
+
+// Oinker
+socket.on('oinker', function(player) {
+    $('#rollMessage').html(`${player.name} got an oinker! Total points go back to zero.`)
+    $('#rollMessage').show()
+    $('#rollButton').hide()
+})
+
+// Pig Out
+socket.on('pig out', function(player) {
+    $('#rollMessage').html(`${player.name} got a pig out! You get zero points this turn.`)
+    $('#rollMessage').show()
+    $('#rollButton').hide()
+})
+// Winner
+socket.on('winner', function(player) {
+    console.log("made it to winner")
+    $(document).ready(function() {
+        $('#turn').hide()
+        $('#titleMessage').html(`${player.name} won the game!!!`)
+        $('#titleMessage').show()
+    })
+});
+// Reset the game
+$(document).ready(function() {
+    $(document).ready(function() {
+        $('#resetButton').click(function(event) {
+            console.log("reset button clicked")
+            socket.emit('reset game')
+        })
+    })
+})  
+socket.on('reset', function() {
+    $(document).ready(function() {
+        $("#gamePrep").show()
+        $('.message').html("")
+        $('#turn').hide()
+        $('#startGame').show()
+        $('#startButton').hide()
+    })
+})
